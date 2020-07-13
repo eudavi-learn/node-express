ffdb38a16022a29d8e88768a5ba9b6cf3b5e5676
Davi <davi.guimaraesleite@gmail.com>
2020-07-13 17:18:09 -0300

    :star2: feat: use sendFile method to render index.html.

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  res.sendFile( __dirname + "/views/index.html");
 });
 
  module.exports = app;
